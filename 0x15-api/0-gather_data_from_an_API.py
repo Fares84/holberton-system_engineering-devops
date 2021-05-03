daa7207fd7b83114016da387c003521a503b74bc
Fares84 <fares.sassi2015@gmail.com>
2021-05-03 21:37:00 +0000

    fixed files and pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-""" Python script using REST API for a given
-employee ID, returns information about TODO list progress """
+""" Python script using REST API
+for a given employee ID, returns
+information about TODO list progress """
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
     todos = requests.get("https://jsonplaceholder.typicode.com/todos/",
-                         params={"user_id": argv[1]})
+                         params={"userId": argv[1]})
     users = requests.get("https://jsonplaceholder.typicode.com/users/",
-                         params={"user_id": argv[1]})
+                         params={"id": argv[1]})
     tasks = todos.json()
     user = users.json()
     for item in user:
         name = item.get("name")
-    task_end = 0
+    completed_tasks = 0
     for task in tasks:
         if task.get("completed"):
-            task_end += 1
-    print("Employee {} is done with tasks({}/{}:".format(name,
-                                                         task_end,
-                                                         len(tasks)))
+            completed_tasks += 1
+    print("Employee {} is done with tasks({}/{}):".format(name,
+                                                          completed_tasks,
+                                                          len(tasks)))
     for title in tasks:
         if title.get("completed"):
             print("\t {}".format(title.get("title")))
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,31 +7,30 @@ from sys import argv
 
 if __name__ == "__main__":
     todos = requests.get("https://jsonplaceholder.typicode.com/todos/",
-                         params={"user_id": argv[1]})
+                         params={"userId": argv[1]})
     users = requests.get("https://jsonplaceholder.typicode.com/users/",
-                         params={"user_id": argv[1]})
+                         params={"id": argv[1]})
     tasks = todos.json()
     user = users.json()
     for item in user:
         name = item.get("name")
-        username = item.get("username")
+        username = item.get('username')
 
-    task_end = 0
+    end_tasks = 0
     for task in tasks:
         if task.get("completed"):
-            task_end += 1
-    print("Employee {} is done with tasks({}/{}:".format(name,
-                                                         task_end,
-                                                         len(tasks)))
+            end_tasks += 1
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, end_tasks, len(tasks)))
     for title in tasks:
         if title.get("completed"):
-            print("\t {}".format(title.get("title")))
+            print(title.get("title"))
 
-with open('{}.csv'.format(argv[1]), 'w') as file:
-    dataFile = task.get('title')
-    writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-    for task in tasks:
-        writer.writerow([argv[1],
-                         username,
-                         task.get("completed"),
-                         task.get("title")])
+    with open('{}.csv'.format(argv[1]), 'w') as f:
+        data_file = task.get('title')
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            writer.writerow([argv[1],
+                             username,
+                             task.get("completed"),
+                             task.get("title")])
